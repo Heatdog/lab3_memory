304ece3ff2bff01ddea6fccbf20e1a4c5d49b8da
Heatdog <71334663+Heatdog@users.noreply.github.com>
2021-04-28 18:54:28 +0300

    Добавьте файлы проекта.

---
+++ lab3_memory/main.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h> 
+
+#include <stdio.h>
+#include "hash.h"
+#include "for_all.h"
+
+
+const int size = 7;
+
+int main() {
+    Table* table = new_table(size);
+    dialog(table);
+    printf("That's all. Bye!\n");
+    del_table(table);
+    _CrtDumpMemoryLeaks();
+    return 0;
+}
